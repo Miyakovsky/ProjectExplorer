e52468babae7a075384bde270f00a3ab3f10e361
Miyakovsky <yamaneko116@yahoo.co.jp>
2018-08-22 01:45:55 +0900

    追加

---
+++ Assets/Editor/ProjectExplorer/Editor/ProjectExplorer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ProjectExplorer
+{
+    public class ProjectExplorer : EditorWindow
+    {
+        private const string WINDOW_MENU_NAME = "Tools/ProjectExplorer";
+        private const string WINDOW_NAME = "ProjectExplorer";
+
+        [MenuItem(WINDOW_MENU_NAME)]
+        static void WindowOpen()
+        {
+            ProjectExplorer window = GetWindow<ProjectExplorer>(WINDOW_NAME);
+            window.Show();
+        }
+
+        void OnGUI()
+        {
+            EditorGUILayout.LabelField("Example Label");
+        }
+
+
+
+
+
+    }
+}
